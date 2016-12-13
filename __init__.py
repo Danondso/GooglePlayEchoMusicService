0b664387226730baa9785b9c233d208fdb87a47b
Dublin Anondson <Stratophone2112@gmail.com>
2016-12-13 07:06:33 -0600

    First Commit
    Song streaming and queueing are functional, super duper alpha at the
    moment. I'll try to get instructions up here for testing as I'm not sure
    if I'll ever be able to get this thing published right now.

--- __init__.py
+++ __init__.py
@@ -1,18 +1,75 @@
 from gmusicapi import Mobileclient
-from flask import Flask
+import logging
+
+from flask import Flask, render_template
+
+from flask_ask import Ask, audio, statement
 
 app = Flask(__name__)
 
 api = Mobileclient()
 
-def search(query):
-    if query is not None:
-        return api.search(query.encode("UTF-8"))
+ask = Ask(app, "/")
 
-def retrieve_single_song_result(search_result):
-    return api.get_stream_url(search_result['song_hits'][0]['track']['nid'], quality=u'hi')
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-search_result = search("Don't fear the reaper")
-stream_result = retrieve_single_song_result(search_result)
 
-print(stream_result)
+# Build a credentials storage setup or something, fuck this plaintext shit
+def login():
+    return api.login('email', 'password', 'android_id')
+
+
+# def retrieve_single_song_result(query):
+@ask.launch
+def initialize_echo_play():
+    if login() is True:
+        return statement(render_template('welcome')) \
+            .simple_card(title="Echo Play", content="Logged in successfully.")
+    else:
+        return statement(render_template('login_failure')) \
+            .simple_card(title="Echo Play",
+                         content="Failed to login.")  # TODO maybe print out the exception if there is one?
+
+
+@ask.intent("PlaySingleSongIntent")
+def play_single_song(query):
+    search_result = api.search(query)
+
+    stream_url = api.get_stream_url(search_result['song_hits'][0]['track']['nid'], quality=u'hi')
+    song_info = "Playing " + search_result['song_hits'][0]['track']['title'] \
+                + " by " \
+                + search_result['song_hits'][0]['track']['artist']
+    return audio(speech=song_info).play(stream_url=stream_url) \
+        .simple_card(title="Google Music",
+                     content=song_info)
+
+
+@ask.intent("EnqueueSongIntent")
+def enqueue_song(queue_query):
+    search_result = api.search(queue_query)
+    stream_url = api.get_stream_url(search_result['song_hits'][0]['track']['nid'], quality=u'hi')
+    return audio().enqueue(stream_url=stream_url)
+
+
+@ask.intent("PlayNextIntent")
+def play_next():
+    return True
+
+
+@ask.intent("AMAZON.PauseIntent")
+def pause_song():
+    return audio().stop()
+
+
+@ask.intent("AMAZON.ResumeIntent")
+def resume_song():
+    return audio().resume()  # Restarts song when resumed, need to fix that
+
+
+@ask.intent("SkipSongIntent")
+def skip_song():
+    return audio(speech="paused").s
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
