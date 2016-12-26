9a5c8c7c84d9e58b03b9190942d6637354314342
PostsDesert <gbaker@student.csdspartans.org>
2016-12-26 18:33:33 -0500

    added loginCredentials.py to store login and password (This will make it alot easier to develop for)

---
+++ loginCredentials.py
@@ -0,0 +1,5 @@
+# Put your email in the "EMAIL" field and your password in the "PASSWORD" field
+username = "EMAIL"
+password = "PASSWORD"
+# Build a credentials storage setup or something, fuck this plaintext shit
+# (I've already send the password to a friend on accendent)
